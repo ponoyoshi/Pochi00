a73578fdc358b42934e36b4ac32a18976c246a1e
Tochi <tochi_osu@hotmail.com>
2019-07-06 19:23:54 +0200

    HighlightManager

--- HighlightManager.cs
+++ HighlightManager.cs
@@ -16,8 +16,65 @@ namespace StorybrewScripts
     {
         public override void Generate()
         {
-		    
+            // Section 1
+            HighlightEffect(8, 6678, 28011, "sb/cf.png", 0.5f, 0.8f, 1000, 1, OsbEasing.OutExpo, true);
             
+            // Section 14
+            HighlightEffect(8, 256385, 272937, "sb/cf.png", 0.5f, 0.8f, 1000, 1, OsbEasing.OutExpo, true);
+            
+            // Section 19
+            HighlightEffect(8, 325902, 332523, "sb/cf.png", 0.5f, 0.8f, 1000, 1, OsbEasing.OutExpo, true);
+        }
+
+        public void HighlightEffect(int BeatDivisor, int StartTime, int EndTime, string SpritePath, float StartScale, float EndScale, int FadeTime, float Fade, OsbEasing Easing, bool UseHitobjectColor)
+        {
+		    var hitobjectLayer = GetLayer("");
+            foreach (var hitobject in Beatmap.HitObjects)
+            {
+                if ((StartTime != 0 || EndTime != 0) && 
+                    (hitobject.StartTime < StartTime - 5 || EndTime - 5 <= hitobject.StartTime))
+                    continue;
+
+                var sprite = hitobjectLayer.CreateSprite(SpritePath, OsbOrigin.Centre, hitobject.Position);
+                sprite.Additive(hitobject.StartTime, hitobject.EndTime + FadeTime);
+
+                if (UseHitobjectColor)
+                {
+                    sprite.Color(hitobject.StartTime, hitobject.Color);
+                }
+
+                else
+                {
+                    sprite.Color(hitobject.StartTime, Color4.White);
+                }
+
+                if (hitobject is OsuSlider)
+                {
+                    var timestep = Beatmap.GetTimingPointAt((int)hitobject.StartTime).BeatDuration / BeatDivisor;
+                    var startTime = hitobject.StartTime;
+                    while (true)
+                    {
+                        var endTime = startTime + timestep;
+
+                        var complete = hitobject.EndTime - endTime < 5;
+                        if (complete) endTime = hitobject.EndTime;
+
+                        var startPosition = sprite.PositionAt(startTime);
+                        sprite.Move(startTime, endTime, startPosition, hitobject.PositionAtTime(endTime));
+                        sprite.Fade(Easing, hitobject.EndTime, hitobject.EndTime + FadeTime, Fade, 0);
+                        sprite.Scale(Easing, hitobject.EndTime, hitobject.EndTime + FadeTime, StartScale, EndScale);
+
+                        if (complete) break;
+                        startTime += timestep;
+                    }
+                }
+
+                else
+                {
+                    sprite.Fade(Easing, hitobject.StartTime, hitobject.EndTime + FadeTime, Fade, 0);
+                    sprite.Scale(Easing, hitobject.StartTime, hitobject.EndTime + FadeTime, StartScale, EndScale);
+                }
+            }        
         }
     }
 }
