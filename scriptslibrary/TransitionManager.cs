53dce79d9016fda447965ce302ce271dd9428762
Tochi <tochi_osu@hotmail.com>
2019-08-04 00:25:59 +0200

    Revert "Update TransitionManager.cs"
    This reverts commit a4328e4641e6b7806c74c65a955c4c2205b0d8b1.

--- scriptslibrary/TransitionManager.cs
+++ scriptslibrary/TransitionManager.cs
@@ -30,4 +30,63 @@ public class TransitionManager
             posX += 900/60;
         }
     }
+    public void SquareTransition(int startTime, int endTime, bool In, int squareScale, Color4 color, OsbEasing easing)
+    {
+        int posX = -107;
+        int posY = 0;
+
+        while(posX < 747 + squareScale)
+        {
+            while(posY < 480 + squareScale)
+            {
+                var sprite = generator.GetLayer("TRANSITION").CreateSprite("sb/p.png", OsbOrigin.Centre, new Vector2(posX, posY));
+                sprite.Fade(startTime, endTime, 1, 1);
+                
+                if(In)
+                {
+                    sprite.Scale(easing, startTime, endTime, 0, squareScale);
+                    sprite.Rotate(easing, startTime, endTime, Math.PI, 0);
+                }
+                else
+                {
+                    sprite.Scale(easing, startTime, endTime, squareScale, 0);
+                    sprite.Rotate(easing, startTime, endTime, 0, -Math.PI);        
+                }
+                sprite.Color(startTime, color);
+                posY += squareScale;
+            }
+            posY = 0;
+            posX += squareScale;
+        }
+    }
+    public void SquareTransitionScaled(string spritePath, int startTime, int endTime, bool In, double scale, Color4 color, OsbEasing easing)
+    {
+        double posX = -107;
+        double posY = 0;
+        Bitmap spriteBitmap = new Bitmap(generator.MapsetPath + "/" + spritePath);
+
+        while(posX < 747 + spriteBitmap.Height*scale)
+        {
+            while(posY < 480 + spriteBitmap.Height*scale)
+            {
+                var sprite = generator.GetLayer("TRANSITION").CreateSprite(spritePath, OsbOrigin.Centre, new Vector2((float)posX, (float)posY));
+                sprite.Fade(startTime, endTime, 1, 1);
+                
+                if(In)
+                {
+                    sprite.Scale(easing, startTime, endTime, 0, scale);
+                    sprite.Rotate(easing, startTime, endTime, Math.PI, 0);
+                }
+                else
+                {
+                    sprite.Scale(easing, startTime, endTime, scale, 0);
+                    sprite.Rotate(easing, startTime, endTime, 0, -Math.PI);        
+                }
+                sprite.Color(startTime, color);
+                posY += spriteBitmap.Height*scale;
+            }
+            posY = 0;
+            posX += spriteBitmap.Height*scale;
+        }
+    }
 }
